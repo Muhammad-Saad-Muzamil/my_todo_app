414cc0d09f06f2022d8358b4826171a1213552b4
Muhammad Saad Muzamil <muhammadsaadmuzamil@gmail.com>
2024-11-13 20:36:38 +0500

    updated app

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,32 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_todo_app/firebase_options.dart';
 import 'package:my_todo_app/screens/main_app_page.dart';
-import 'package:my_todo_app/screens/login_page.dart';
-import 'package:my_todo_app/screens/signUp_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Your Todo App',
-      initialRoute: '/login', // You can set the initial route here
+      initialRoute: '/mainApp', // You can set the initial route here
       routes: {
-        '/login': (context) => LoginScreen(),
-        '/signup': (context) => SignUpScreen(),
+  
         '/mainApp': (context) => MainAppScreen(),
       },
-      home: StreamBuilder<User?>(
-        stream: _auth.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else if (snapshot.hasData) {
-            print("User authenticated: ${snapshot.data?.email}");
-            return MainAppScreen();
-          } else {
-            return LoginScreen();
-          }
-        },
-      ),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Placeholder for determining initial screen logic if needed
+    return MainAppScreen();
   }
 }
